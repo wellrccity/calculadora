7c8392eea4da4c7378e96f20b1a34f7b46a44cf1
wellington <wellington>
2023-10-25 11:16:23 -0300

    calculadora

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input';
+import Button from './components/Button';
+import { useState } from 'react';
 
-function App() {
+import { Container, Content, Row, Column } from './styles'
+
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+  }
+  const handleAddNumber = (num) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
+  }
+
   return (
-    <>Hello World</>
-
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>                                  
+            <Button label='√' onClick={() => handleAddNumber('')}/>
+            <Button label='X' onClick={() => handleAddNumber('')} />
+            <Button label='÷' onClick={() => handleAddNumber('')}/>
+            <Button label='C' onClick={() => handleOnClear()}/>
+          </Row>
+        <Row>
+            <Button label='7' onClick={() => handleAddNumber('7')} />
+            <Button label='8' onClick={() => handleAddNumber('8')}/>
+            <Button label='9' onClick={() => handleAddNumber('9')}/>
+            <Button label='-' onClick={() => handleAddNumber('-')}/>
+          </Row>
+         <Row>
+            <Button label='4' onClick={() => handleAddNumber('4')} />
+            <Button label='5' onClick={() => handleAddNumber('5')}/>
+            <Button label='6' onClick={() => handleAddNumber('6')}/>
+            <Button label='+' onClick={() => handleAddNumber('+')}/>
+          </Row>
+          <Row>
+            <Button label='1' onClick={() => handleAddNumber('1')} />
+            <Button label='2' onClick={() => handleAddNumber('2')}/>
+            <Button label='3' onClick={() => handleAddNumber('3')}/>
+            <Button label='=' onClick={() => handleAddNumber('=')}/>
+          </Row>
+          <Row>          
+            <Button label='0' onClick={() => handleAddNumber('0')}/>
+          </Row>
+          
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFFAA;
+        border: 0;
+        direction: rtl;
+        orientation: initial;
+
+        font-size: 60px;
+        font-family:  'Roboto';
+        color: #FFFFFF;
+    }
+`
